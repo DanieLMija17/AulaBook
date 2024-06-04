cdfdb4dd4b91bceccce5e26c5575cd85651f6978
Rolf2020 <rodolfomarin2020@gmail.com>
2024-06-04 17:41:15 -0400

    vista aula responsive

--- aulabook/lib/AulaInfo/aula.dart
+++ aulabook/lib/AulaInfo/aula.dart
@@ -53,6 +53,10 @@ class _DescriptionWidgetState extends State<DescriptionWidget> {
 
   @override
   Widget build(BuildContext context) {
+    // Obtenemos el tamaño de la pantalla
+    var screenSize = MediaQuery.of(context).size;
+    var screenWidth = screenSize.width;
+
     return SingleChildScrollView(
       padding: const EdgeInsets.all(16.0),
       child: Column(
@@ -62,22 +66,22 @@ class _DescriptionWidgetState extends State<DescriptionWidget> {
             borderRadius: BorderRadius.circular(20.0),
             child: Image.asset(
               'assets/salon_unimet1.jpg',
-              width: 335,
-              height: 300, // Ajustamos la altura para que entre en la pantalla
+              width: screenWidth * 0.9, // Ajustamos el ancho relativo al tamaño de la pantalla
+              height: screenWidth * 0.8, // Ajustamos la altura relativa al tamaño de la pantalla
               fit: BoxFit.cover,
             ),
           ),
           SizedBox(height: 16), // Espacio entre la imagen y el texto "A1-204"
           Text(
             'A1-204',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: screenWidth * 0.06, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 8),
           Row(
             children: [
-              Icon(Icons.star, color: Colors.yellow, size: 16),
+              Icon(Icons.star, color: Colors.yellow, size: screenWidth * 0.04),
               SizedBox(width: 4),
-              Text('4.5 (355 Reviews)', style: TextStyle(fontSize: 16)),
+              Text('4.5 (355 Reviews)', style: TextStyle(fontSize: screenWidth * 0.04)),
             ],
           ),
           SizedBox(height: 8),
@@ -85,7 +89,7 @@ class _DescriptionWidgetState extends State<DescriptionWidget> {
             _isExpanded
                 ? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget porta tellus, non ultricies risus. Nam et metus eget ipsum tempor consequat et eget risus. Donec vel felis euismod, gravida nisi nec, ultrices ipsum. Integer pulvinar odio ac ligula dignissim, non luctus metus tempus. Phasellus malesuada libero nec nisl fermentum, eget tristique urna bibendum. Suspendisse vel libero vel purus consectetur dictum. Cras non scelerisque felis. Vestibulum sit amet sapien semper, aliquet felis sit amet, fringilla leo. Nulla facilisi. Nulla at dui non urna consequat facilisis. Mauris aliquet dui id arcu vulputate, ac feugiat nisl dapibus. Ut id nunc in ex malesuada condimentum.'
                 : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget porta tellus, non ultricies risus. Nam et metus eget ipsum tempor consequat et eget risus....',
-            style: TextStyle(fontSize: 16),
+            style: TextStyle(fontSize: screenWidth * 0.04),
           ),
           SizedBox(height: 16),
           Align(
@@ -114,22 +118,22 @@ class _DescriptionWidgetState extends State<DescriptionWidget> {
           SizedBox(height: 16),
           Text(
             'Comodidades',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: screenWidth * 0.05, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 8),
           Row(
             children: [
-              Icon(Icons.wifi),
+              Icon(Icons.wifi, size: screenWidth * 0.06),
               SizedBox(width: 8),
-              Text('Wifi', style: TextStyle(fontSize: 16)),
+              Text('Wifi', style: TextStyle(fontSize: screenWidth * 0.04)),
             ],
           ),
           SizedBox(height: 8),
           Row(
             children: [
-              Icon(Icons.group),
+              Icon(Icons.group, size: screenWidth * 0.06),
               SizedBox(width: 8),
-              Text('26 Puestos', style: TextStyle(fontSize: 16)),
+              Text('26 Puestos', style: TextStyle(fontSize: screenWidth * 0.04)),
             ],
           ),
           SizedBox(height: 8),
@@ -138,18 +142,18 @@ class _DescriptionWidgetState extends State<DescriptionWidget> {
             children: [
               Column(
                 children: [
-                  Icon(Icons.business),
+                  Icon(Icons.business, size: screenWidth * 0.06),
                   SizedBox(height: 8),
-                  Icon(Icons.ac_unit),
+                  Icon(Icons.ac_unit, size: screenWidth * 0.06),
                 ],
               ),
               SizedBox(width: 8),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('2 Pizarras', style: TextStyle(fontSize: 16)),
+                  Text('2 Pizarras', style: TextStyle(fontSize: screenWidth * 0.04)),
                   SizedBox(height: 8),
-                  Text('Aire', style: TextStyle(fontSize: 16)),
+                  Text('Aire', style: TextStyle(fontSize: screenWidth * 0.04)),
                 ],
               ),
             ],
@@ -157,14 +161,16 @@ class _DescriptionWidgetState extends State<DescriptionWidget> {
           SizedBox(height: 16),
           Center(
             child: SizedBox(
-              width: 330,
-              height: 56,
+              width: screenWidth * 0.85, // Ajustamos el ancho relativo al tamaño de la pantalla
+              height: screenWidth * 0.14, // Ajustamos la altura relativa al tamaño de la pantalla
               child: ElevatedButton(
                 onPressed: () {},
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Color(0xFFFD8204), // Color de fondo
                   foregroundColor: Colors.white, // Color del texto
-                  textStyle: TextStyle(fontSize: 16),
+                  textStyle: TextStyle(fontSize: screenWidth * 0.045),
+                  elevation: 4, // Elevación para el efecto de sombra
+                  shadowColor: Color(0x3D0038FF), // Color de la sombra con 24% de opacidad
                 ),
                 child: Text('Reservar'),
               ),
@@ -175,6 +181,8 @@ class _DescriptionWidgetState extends State<DescriptionWidget> {
     );
   }
 }
+
+
 
 
 
