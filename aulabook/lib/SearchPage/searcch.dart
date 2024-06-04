731c52ab5c24397abec968f9f2da5b3101351bd3
Andres Leonardo Balbuena Valera <77251658+AndresBLV@users.noreply.github.com>
2024-06-04 11:20:17 -0400

    Vistas prototipo

---
+++ aulabook/lib/SearchPage/searcch.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MySearch());
+
+class MySearch extends StatelessWidget {
+  const MySearch({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SearchPage(),
+    );
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  final List<Category> categories = [
+    Category(icon: Icons.class_, label: 'Salon'),
+    Category(icon: Icons.science, label: 'Laboratorio'),
+    Category(icon: Icons.apartment, label: 'Modulo'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search'),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.orange,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Buscar',
+                prefixIcon: Icon(Icons.search, color: Colors.orange),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: GridView.builder(
+                itemCount: categories.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 3 / 2,
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 20,
+                ),
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Icon(categories[index].icon,
+                          size: 40, color: Colors.orange),
+                      SizedBox(height: 8),
+                      Text(categories[index].label,
+                          style: TextStyle(color: Colors.orange)),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Category {
+  final IconData icon;
+  final String label;
+
+  Category({required this.icon, required this.label});
+}
