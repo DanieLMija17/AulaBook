7e7df896782a483d48fbe0060c5ebebf72911ddb
HMontserrat <hector.montserrat@zkymed.com>
2024-07-09 08:32:29 -0400

    vamo a ver

--- aulabook/lib/HomePage/login.dart
+++ aulabook/lib/HomePage/login.dart
@@ -4,16 +4,14 @@ import 'package:aulabook/Componentes/custom_button.dart';
 import 'package:aulabook/Componentes/help_button.dart';
 import 'package:aulabook/HomePage/register.dart';
 import 'package:aulabook/SearchPage/search.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:aulabook/HomePage/auth.dart';
 
-void main() => runApp(const LoginScreenApp());
-
-// AQUIIII
-// class _LoginPageState extends State<LoginPage> {
-//   String? errorMessage = '';
-//   bool isLogin = true;
-//   }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const LoginScreenApp());
+}
 
 class LoginScreenApp extends StatelessWidget {
   const LoginScreenApp({super.key});
@@ -43,172 +41,189 @@ class LoginScreenApp extends StatelessWidget {
           secondary: Color(0xFFFD8204),
         ),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('AulaBook'),
-          automaticallyImplyLeading: false,
-          leading: Padding(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: const Color(0xFFF3F8FE),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: IconButton(
-                  icon: const Icon(Icons.arrow_back, color: Color(0xFFB8B8B8)),
-                  onPressed: () {
-                    print('Back button pressed');
-                    Navigator.pop(context); // Navigate back to the previous screen
-                  },
-                  iconSize: 24,
-                  padding: EdgeInsets.zero,
-                  constraints: const BoxConstraints(
-                    minHeight: 40,
-                    minWidth: 40,
-                  ),
-                  splashRadius: 24,
-                ),
-              ),
-            ),
-          ),
-          actions: const [
-            Padding(
-              padding: EdgeInsets.only(right: 8.0),
-              child: HelpButton(),
-            ),
-          ],
-        ),
-        body: const LoginScreen(),
-      ),
+      home: const LoginScreen(),
     );
   }
 }
 
-
-
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool isLoading = false;
+
+  Future<void> _signInWithEmailPassword() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+      User? user = userCredential.user;
+      print('User signed in: ${user?.email}'); // Ejemplo de uso de la variable
+
+      // Usuario ha iniciado sesión correctamente
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const MySearch()),
+      );
+    } on FirebaseAuthException catch (e) {
+      print('Error: $e');
+      // Manejar errores
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     var screenSize = MediaQuery.of(context).size;
     var screenWidth = screenSize.width;
 
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'Iniciar Sesión',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: screenWidth * 0.08,
-                  fontWeight: FontWeight.bold,
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AulaBook'),
+        automaticallyImplyLeading: false,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFFF3F8FE),
+                borderRadius: BorderRadius.circular(8),
               ),
-              const SizedBox(height: 30),
-              Text(
-                'Ingresar su correo electrónico',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: screenWidth * 0.05,
-                  color: Colors.black,
-                ),
-              ),
-              const SizedBox(height: 10),
-              const TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Correo electrónico',
-                ),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                'Ingrese su contraseña',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: screenWidth * 0.05,
-                  color: Colors.black,
-                ),
-              ),
-              const SizedBox(height: 10),
-              const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Contraseña',
-                ),
-              ),
-              const SizedBox(height: 20),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const RegisterScreenApp()),
-                  );
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back, color: Color(0xFFB8B8B8)),
+                onPressed: () {
+                  print('Back button pressed');
+                  Navigator.pop(
+                      context); // Navigate back to the previous screen
                 },
-                child: Text(
-                  'No tiene cuenta? Regístrese',
+                iconSize: 24,
+                padding: EdgeInsets.zero,
+                constraints: const BoxConstraints(
+                  minHeight: 40,
+                  minWidth: 40,
+                ),
+                splashRadius: 24,
+              ),
+            ),
+          ),
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: HelpButton(),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Iniciar Sesión',
+                  textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: screenWidth * 0.04,
-                    color: Colors.grey,
-                    decoration: TextDecoration.underline,
+                    fontSize: screenWidth * 0.08,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton.icon(
-                onPressed: () {
-                  // Handle Google login
-                },
-                icon: SvgPicture.asset(
-                  'assets/images/google1.svg', // Make sure this path is correct
-                  width: 24,
-                  height: 24,
-                ),
-                label: Text(
-                  'Iniciar Sesión con Google',
+                const SizedBox(height: 30),
+                Text(
+                  'Ingresar su correo electrónico',
+                  textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: screenWidth * 0.045,
+                    fontSize: screenWidth * 0.05,
                     color: Colors.black,
                   ),
                 ),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black, backgroundColor: Colors.white,
-                  textStyle: TextStyle(
-                    fontSize: screenWidth * 0.045,
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Correo electrónico',
                   ),
-                  minimumSize: Size(screenWidth * 0.85, screenWidth * 0.14),
-                  side: const BorderSide(color: Colors.grey),
                 ),
-              ),
-              const SizedBox(height: 20),
-              CustomButton(
-                width: screenWidth * 0.85,
-                height: screenWidth * 0.14,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const MySearch()),
-                  );
-                },
-                label: 'Siguiente',
-              ),
-            ],
+                const SizedBox(height: 20),
+                Text(
+                  'Ingrese su contraseña',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.05,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Contraseña',
+                  ),
+                ),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RegisterScreenApp()),
+                    );
+                  },
+                  child: Text(
+                    'No tiene cuenta? Regístrese',
+                    style: TextStyle(
+                      fontSize: screenWidth * 0.04,
+                      color: Colors.grey,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                isLoading
+                    ? const CircularProgressIndicator()
+                    : CustomButton(
+                        width: screenWidth * 0.85,
+                        height: screenWidth * 0.14,
+                        onPressed: _signInWithEmailPassword,
+                        label: 'Iniciar Sesión',
+                      ),
+                const SizedBox(height: 20),
+                CustomButton(
+                  width: screenWidth * 0.85,
+                  height: screenWidth * 0.14,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const MySearch()),
+                    );
+                  },
+                  label: 'Siguiente',
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
